24950fa5c925edb5087a02088b2cea1035173085
[chaoxuanxiaok] <[1324163139@qq.com]>
2019-03-07 15:12:31 +0800

    3.7 commit

--- zuul-demo/src/main/java/com/winston/zuuldemo/ZuulDemoApplication.java
+++ zuul-demo/src/main/java/com/winston/zuuldemo/ZuulDemoApplication.java
@@ -2,8 +2,12 @@ package com.winston.zuuldemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy    //开启Zuul的网关功能
+@EnableDiscoveryClient  //开启eureka客户端发现功能
 public class ZuulDemoApplication {
 
     public static void main(String[] args) {
